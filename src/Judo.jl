80797429962f4cdb0302e6101250723a9ef1da10
Daniel Jones <dcjones@cs.washington.edu>
2013-08-10 20:23:54 -0700

    Displaying and executing things.

--- src/Judo.jl
+++ src/Judo.jl
@@ -1,6 +1,7 @@
 
 module Judo
 
+import Base: start, next, done, display
 import JSON
 import YAML
 
@@ -33,6 +34,11 @@ end
 
 function done(it::ParseIt, pos)
     pos > length(it.value)
+end
+
+
+# A special dummy module in which a documents code is executed.
+module WeaveSandbox
 end
 
 
@@ -74,6 +80,106 @@ function string_to_pandoc_json(s::String)
 end
 
 
+# A Display implementation that renders multimedia into the document currently
+# being processed.
+type WeaveDoc <: Display
+    # JSON respresentation of the document being built up.
+    blocks::Array
+
+    # Queued output from display calls
+    display_blocks::Array
+
+    # Document name (used for naming any external files generated)
+    name::String
+
+    # Output formal
+    outfmt::Symbol
+
+    # Current figure number
+    fignum::Int
+
+    # Redirected stdout
+    stdout_read
+    stdout_write
+
+    function WeaveDoc(name::String, outfmt::Symbol, stdout_read, stdout_write)
+        new({}, {}, name, outfmt, 1, stdout_read, stdout_write)
+    end
+end
+
+
+# Display functions that render output and insert them into the document.
+function display(doc::WeaveDoc, ::@MIME("text/plain"), data)
+    block =
+        {"CodeBlock" =>
+           {{"", {}, {}}, data}}
+    push!(doc.display_blocks, block)
+end
+
+
+function display(doc::WeaveDoc, ::@MIME("text/latex"), data)
+    # latex to dvi
+    input_path, input = mktemp()
+    print(input, data)
+    flush(input)
+    seek(input, 0)
+    latexout_dir = mktempdir()
+    run(`latex -output-format=dvi -output-directory=$(latexout_dir) $(input_path)` |> SpawnNullStream())
+    rm(input_path)
+
+    # dvi to svg
+    latexout_path = "$(latexout_dir)/$(basename(input_path)).dvi"
+    output = readall(`dvisvgm --stdout --no-fonts $(latexout_path)` .> SpawnNullStream())
+    run(`rm -rf $(latexout_dir)`)
+
+    display("image/svg+xml", output)
+end
+
+
+function display(doc::WeaveDoc, ::@MIME("image/svg+xml"), data)
+    filename = @sprintf("%s_figure_%d.svg", doc.name, doc.fignum)
+    out = open(filename, "w")
+    write(out, data)
+    close(out)
+
+    alttext = @sprintf("Figure %d", doc.fignum)
+    figurl = filename
+    caption = ""
+
+    if doc.outfmt == :html || doc.outfmt == :html5
+        block =
+            {"RawBlock" =>
+              {"html",
+               """
+               <figure>
+                 <object alt="$(alttext)" data="$(figurl)" type="image/svg+xml">
+                 </object>
+                 <figcaption>
+                   $(caption)
+                 </figcaption>
+               </figure>
+               """}}
+    else
+        block =
+            {"Para" =>
+              {{"Image" =>
+                {{{"Str" => alttext}},
+                 {figurl, ""}}}}}
+    end
+
+    doc.fignum += 1
+    push!(doc.display_blocks, block)
+end
+
+
+function display(doc::WeaveDoc, ::@MIME("text/vnd.graphviz"), data)
+    output, input, proc = readandwrite(`dot -Tsvg`)
+    write(input, data)
+    close(input)
+    display("image/svg+xml", readall(output))
+end
+
+
 # Transform a annotated markdown file into a variety of formats.
 #
 # This reads markdown input, optionally prefixed with a YAML metadata document,
@@ -85,26 +191,32 @@ end
 #   output: Where the resulting document should be written.
 #   outfmt: One of the output formats supported by pandoc:
 #       Eg. markdown, rst, html, json, latex.
+#   name: A short name for the document used for naming output files.
 #
-function weave(input::IO, output::IO; outfmt=:html5)
+function weave(input::IO, output::IO; outfmt=:html5, name="judo")
     input_text = readall(input)
 
     # parse yaml front matter
     mat = match(frontmatter_pattern, input_text)
     metadata = nothing
     if !is(mat, nothing)
-        metadata = YAML.load(mat.match)
+        metadata = YAML.load(bytestring(mat.match))
         input_text = input_text[1+length(mat.match):]
     end
 
-
     # first pandoc pass
     pandoc_metadata, document = JSON.parse(pandoc(input_text, :markdown, :json))
-    processed_document = {}
+    prev_stdout = STDOUT
+    stdout_read, stdout_write = redirect_stdout()
+    doc = WeaveDoc(name, outfmt, stdout_read, stdout_write)
+    pushdisplay(doc)
 
     for block in document
-        # TODO: everything
-        push!(processed_document, block)
+        if !isa(block, Dict) || !haskey(block, "CodeBlock")
+            push!(doc.blocks, process_block(block))
+        else
+            process_code_block(doc, block)
+        end
     end
 
     # splice in metadata fields that pandoc supports
@@ -134,13 +246,178 @@ function weave(input::IO, output::IO; outfmt=:html5)
         end
     end
 
+    popdisplay(doc)
+    Base.reinit_stdio()
+
     # second pandoc pass
     buf = IOBuffer()
-    JSON.print(buf, {pandoc_metadata, processed_document})
+    JSON.print(buf, {pandoc_metadata, doc.blocks})
     write(output, pandoc(takebuf_string(buf), :json, outfmt))
 end
 
 
+# Evaluate the truthiness of a code block attribute.
+function codeblock_keyval_bool(keyvals::Dict, key, default::Bool)
+    haskey(keyvals, key) ?
+        lowercase(strip(keyvals[key])) != "false" : default
 end
 
 
+# Process a text paragraph.
+#
+# Args:
+#   block: Block parsed from the json document representation.
+#
+# Modifies:
+#    block, prior to inserting it into doc
+#
+# Returns:
+#   block
+#
+function process_block(block::Dict)
+    for (k, v) in block
+        if k == "Code"
+            (id, classes, keyvals), text = v
+            if contains(classes, "julia")
+                out = eval(WeaveSandbox, parse(text))
+                v[2] = string(out)
+            end
+        else
+            block[k] = process_block(v)
+        end
+    end
+    block
+end
+
+
+function process_block(block::Array)
+    for i in 1:length(block)
+        block[i] = process_block(block[i])
+    end
+    block
+end
+
+
+function process_block(block::Any)
+    block
+end
+
+
+
+# Code block classes supported.
+const code_block_classes = Set(
+    "julia",
+    "graphviz",
+    "latex",
+    "svg"
+)
+
+
+
+# Process code blocks.
+#
+# This consists of (optioclasses =ally) executing code and inserting the results into
+# the document.
+#
+# Args:
+#   doc: Document being generated.
+#   block:: Code block.
+#
+function process_code_block(doc::WeaveDoc, block::Dict)
+    (id, classes, keyvals_array), text = block["CodeBlock"]
+    keyvals = [k => v for (k, v) in keyvals_array]
+
+    # Options are:
+    #
+    #  hide: Don't show the code block. (defaut: false)
+    #  execute: Do excute the code block. (default: true)
+    #  display: Do display output. (default: true)
+    #  results:
+    #    none (default)
+    #    block
+    #    expression
+
+    keyvals["hide"]    = codeblock_keyval_bool(keyvals, "hide",    false)
+    keyvals["execute"] = codeblock_keyval_bool(keyvals, "execute", true)
+    keyvals["display"] = codeblock_keyval_bool(keyvals, "display", true)
+    keyvals["results"] = get(keyvals, "results", "none")
+
+    if isempty(classes) || classes[1] == "julia" ||
+       !has(code_block_classes, classes[1])
+
+       if keyvals["results"] == "none" || keyvals["results"] == "block"
+           result = string(nothing)
+           if keyvals["execute"]
+               for (cmd, ex) in parseit(strip(text))
+                   result = safeeval(ex)
+               end
+           end
+
+           if keyvals["results"] == "block"
+               output_text = string(output_text, "\n\n## ", result, "\n")
+           else
+               output_text = text
+           end
+       elseif keyvals["results"] == "expression"
+           if keyvals["execute"]
+               output_text = IOBuffer()
+               for (cmd, ex) in parseit(strip(text))
+                   println(output_text, cmd)
+                   println(output_text, "## ", safeeval(ex), "\n")
+               end
+           else
+               output_text = text
+           end
+       end
+
+       if !keyvals["hide"]
+           push!(doc.blocks,
+           {"CodeBlock" => {{id, {"julia", classes...}, keyvals_array}, output_text}})
+       end
+
+       if keyvals["display"]
+           # TODO: Is there a way to check for output without this dirty trick?
+           write(doc.stdout_write, '.')
+           stdout_output = readavailable(doc.stdout_read)[1:end-1]
+           if !isempty(stdout_output)
+               display("text/plain", stdout_output)
+           end
+           append!(doc.blocks, doc.display_blocks)
+       end
+       empty!(doc.display_blocks)
+    else
+       if !hide
+           push!(doc.blocks, block)
+       end
+
+       class = classes[1]
+       if display
+           if class == "graphviz"
+               display("text/vnd.graphviz", text)
+           elseif class == "latex"
+               display("text/latex", text)
+           elseif class == "svg"
+               display("image/svg+xml", text)
+           end
+       end
+   end
+end
+
+
+# Evaluate an expression and return its results. Catch any errors and return
+# them in string form.
+function safeeval(ex::Expr)
+    try
+        string(eval(WeaveSandbox, ex))
+    catch e
+        errout = IOBuffer()
+        print(errout, "ERROR: ")
+        Base.error_show(errout, e)
+        result = bytestring(errout)
+        close(errout)
+        result
+    end
+end
+
+
+end
